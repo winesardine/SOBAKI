bedd1d91f4729d5c7f85b0151d6ffacc7a806210
winesardine <34769270+winesardine@users.noreply.github.com>
2017-12-25 02:08:35 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,24 +68,5 @@
 						.css('transition', 'none');
 
 	});
-	var myform = $("form#myform");
-myform.submit(function(event){
-	event.preventDefault();
-
-  // Change to your service ID, or keep using the default service
-  var service_id = "default_service";
-  var template_id = "template_WxSCxNKc";
-
-  myform.find("button").text("Отправляем...");
-  emailjs.sendForm(service_id,template_id,"myform")
-  	.then(function(){ 
-    	alert("Sent!");
-       myform.find("button").text("Ваше сообщение отправлено!");
-    }, function(err) {
-       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
-       myform.find("button").text("Send");
-    });
-  return false;
-});
 
 })(jQuery);
